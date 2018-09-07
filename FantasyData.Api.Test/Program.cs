31ad62c38ee778fab12f73efdb9dd3b881b2d30f
Scott Gimpel <ScottGimpel@users.noreply.github.com>
2018-09-07 14:09:56 -0400

    updated nuget info

--- FantasyData.Api.Test/Program.cs
+++ FantasyData.Api.Test/Program.cs
@@ -9,8 +9,8 @@ namespace FantasyData.Api.Test
         static void Main(string[] args)
         {
             // Connect to client and get data
-            var client = new MLBv3ProjectionsClient("<API key goes here>");
-            var projections = client.GetPlayerGameProjectionStatsByDate("2018-03-29").OrderByDescending(p => p.DraftKingsSalary).Take(20).ToList();
+            var client = new MLBv3ProjectionsClient("api_key_goes_here");
+            var projections = client.GetPlayerGameProjectionStatsByDate("2018-09-07").OrderByDescending(p => p.DraftKingsSalary).Take(20).ToList();
 
             // Write data to console
             foreach(var projection in projections)
