617c64ba9b46124d28877c2d2abc41cf013c1487
Michael Brown <micbrown@microsoft.com>
2019-10-30 12:57:51 -0700

    converted the library to dotnetstandard

--- FantasyData.Api.Client/Clients/BaseClient.cs
+++ FantasyData.Api.Client/Clients/BaseClient.cs
@@ -1,8 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Runtime.Serialization.Json;
+using Newtonsoft.Json;
 using System.Text;
-using System.Web.Script.Serialization;
 
 namespace FantasyData.Api.Client
 {
@@ -69,8 +68,7 @@ namespace FantasyData.Api.Client
 
                 // Download json, deserialize it, and return it
                 var json = client.DownloadString(url);
-                var serializer = new JavaScriptSerializer() { MaxJsonLength = int.MaxValue };
-                return serializer.Deserialize<T>(json);
+                return JsonConvert.DeserializeObject<T>(json);
                 
             }
 
