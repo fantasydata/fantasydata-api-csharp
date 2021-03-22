403c5089aeb6c0d0673fa3dfb12d9d0c9fdafc23
Scott Gimpel <ScottGimpel@users.noreply.github.com>
2021-03-22 16:13:28 -0400

    updated nuspec

--- FantasyData.Api.Test/Program.cs
+++ FantasyData.Api.Test/Program.cs
@@ -9,27 +9,8 @@ namespace FantasyData.Api.Test
         static void Main(string[] args)
         {
             // Connect to client and get data
-            var client = new  NFLv3ScoresClient("4512f3fb84064c699b18704f70f1708c");
+            var client = new  NFLv3ScoresClient("<api_key_goes_here>");
             var scores = client.GetScores("2020POST");
-            scores = client.GetScores("2020POST");
-            scores = client.GetScores("2020POST");
-            scores = client.GetScores("2020POST");
-            scores = client.GetScores("2020POST");
-            scores = client.GetScores("2020POST");
-            scores = client.GetScores("2020POST");
-            scores = client.GetScores("2020POST");
-            scores = client.GetScores("2020POST");
-            scores = client.GetScores("2020POST");
-            scores = client.GetScores("2020POST");
-            scores = client.GetScores("2020POST");
-            scores = client.GetScores("2020POST");
-            scores = client.GetScores("2020POST");
-            scores = client.GetScores("2020POST");
-            scores = client.GetScores("2020POST");
-            scores = client.GetScores("2020POST");
-            scores = client.GetScores("2020POST");
-            scores = client.GetScores("2020POST");
-            scores = client.GetScores("2020POST");
 
             // Write data to console
             foreach (var score in scores)
