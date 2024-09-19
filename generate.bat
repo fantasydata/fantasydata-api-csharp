e8e27587f5797ceaa9755e72429afe5bd71e8015
Scott Gimpel <scott@fantasydata.com>
2024-09-19 11:35:49 -0400

    updated generate.bat to have better comments

--- generate.bat
+++ generate.bat
@@ -1,5 +1,5 @@
 
-rem TODO: use msbuild to rebuild the solution
+rem TODO: use msbuild to rebuild the solution (or for now, just manually build the solution in Visual Studio in RELEASE mode)
 
 rem Generating Nuget Package
 nuget pack "FantasyData.Api.Client\FantasyData.Api.Client.csproj.nuspec"
